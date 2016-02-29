f349f6dcc32be697602472e82a334598b789f64d
hassamc <hassam.6@live.com>
2016-02-29 12:59:15 -0500

    Added Config File, Initial Config code

---
+++ Uwindsor Formula Hybrid/Formula Hybrid Dashboard/Source/Communication/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiSharp.LibGpio
+using PiSharp.LibGpio.Entities
+
+namespace Formula_Hybrid_Dashboard.Source.Communication
+{
+    class Config
+    {
+        public void Main(string[] args)
+        {
+            // Use test mode
+            LibGpio.Gpio.TestMode = true;
+
+            // Setup pin 11,13 for input (using Physical Board Pin numbers)
+            LibGpio.Gpio.SetupChannel(PhysicalPinNumber.Eleven, Direction.Input);
+            LibGpio.Gpio.SetupChannel(PhysicalPinNumber.Thirteen, Direction.Input);
+
+            // Loop around and set the value of GPIO 0 to the inverse of GPIO 7
+            // ie. If the button is pressed, the LED will go out
+            var oldInput = true;
+            while (true)
+            {
+                var newInput = LibGpio.Gpio.ReadValue(RaspberryPinNumber.Seven);
+                if (newInput != oldInput)
+                {
+                    LibGpio.Gpio.OutputValue(BroadcomPinNumber.Seventeen, !newInput);
+                    oldInput = newInput;
+                }
+            }
+        }
+    }
+}
